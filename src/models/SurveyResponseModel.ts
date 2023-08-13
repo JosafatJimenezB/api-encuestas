9f4ef337414994dc47193675efc7baa562f89cfd
Texhnolyze47 <ichigobankai136@gmail.com>
2023-08-13 12:49:27 -0600

    No necesario ya que se guarda dentro de la misma tabla

--- src/models/SurveyResponseModel.ts
+++ src/models/SurveyResponseModel.ts
@@ -1,7 +1,6 @@
 import ResponseModel from './ResponseModel';
 interface SurveyResponseModel {
-    id: number;
-    surveyId: number; // ID of the survey being responded to
+
     responses: ResponseModel[]; // An array of ResponseModel
     location: {
       latitude: number;
